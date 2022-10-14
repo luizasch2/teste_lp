ea925c09b09d30330dead4215851b9c4b8923b12
Luiza Schneider Sampaio <98579033+luizasch2@users.noreply.github.com>
2022-10-14 08:27:21 -0300

    Update atividade_pizza.py

---
+++ atividade_pizza.py
@@ -0,0 +1,54 @@
+"""
+Dicionarios com quantidade ja estabelecidos
+"""
+
+
+
+def montar(dic1: dict, dic2: dict,dict3:dict, dict4: dict) -> dict:
+    """Montar a pizza e calcular o valor
+
+    :param dic1: dicionario com a massa e quantidade
+    :type dic1: dict
+    :param dic2: dicionario com o molho e quantidade
+    :type dic2: dict
+    :param dict3: dicionario com o queijo e quantidade
+    :type dict3: dict
+    :param dict4: dicionario com o cobertura e quantidade
+    :type dict4: dict
+    :return: pizza montada
+    :rtype: dict
+    """
+
+
+def cadastrar(tipo: str,nome:str,preco: float) -> dict: 
+    """cadastra mais um ingrediente no respectivo dicionário
+
+    :param tipo: tipo do elemento a ser cadastrado
+    :type tipo: str
+    :param nome: nome do ingrediente
+    :type nome: str
+    :param preco: preço do ingrediente
+    :type preco: float
+    :return: dicionario com novo elemento cadastrado
+    :rtype: dict
+    """    
+
+def remover(tipo: str, nome: str) -> dict:
+      """remover  um ingrediente no respectivo dicionário
+
+    :param tipo: tipo do elemento a ser removido
+    :type tipo: str
+    :param nome: nome do ingrediente
+    :type nome: str
+    :return: dicionario com novo elemento removido
+    :rtype: dict
+    """  
+
+def listar(tipo:str) -> dict:
+    """lista de um tipo
+
+    :param tipo: tipo do que quer ser listado
+    :type tipo: str
+    :return: dicionario com tudo listado
+    :rtype: dict
+    """    
